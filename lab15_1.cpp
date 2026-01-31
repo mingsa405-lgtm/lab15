7ae483c6062ec2c88ad8b18bf299311952f5ea82
ming <mingsa405@gmail.com>
2026-01-31 13:45:03 +0700

    up

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,7 +1,25 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+
+	 int a=5;
+   char b='A';
+   char &c=b;
+   int *x=&a;
+   char *y=&b;
+   int **z=&x;
+cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<(int *)y<<" "<<*z<<"\n"; 
+cout<<&a<<" "<<(int *)&b<<" "<<(int*)&c<<" "<<&x<<" "<<&y<<" "<<&z<<"\n"; 
+
+c='F';
+cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<(int *)y<<" "<<*z<<"\n"; 
+*y='W';
+cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<(int *)y<<" "<<*z<<"\n"; 
+*x=6;
+cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<(int *)y<<" "<<*z<<"\n"; 
+**z=7;
+cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<(int *)y<<" "<<*z<<"\n"; 
 
 	
 	
